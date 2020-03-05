1d41f1a23c176ff7c37a7cfc359628c38e4726de
Thomas P <43186116+T-mp@users.noreply.github.com>
2020-03-05 15:54:25 +0100

    ~ Vorbereitung für Unit-Tests

---
+++ src/t-mp.BlazorSyncStorage.Tests/Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace t_mp.BlazorSyncStorage.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestObTestsLaufen()
+        {
+            Assert.Pass();
+        }
+    }
+}
